6148cb4e4f1543a1d0b99aacb4a0332427c73358
Keila-Camarillo <126615506+Keila-Camarillo@users.noreply.github.com>
2023-05-05 16:58:45 -0500

    update

---
+++ wrangle.py
@@ -0,0 +1,150 @@
+import env
+import os
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+from scipy import stats
+from sklearn.model_selection import train_test_split
+
+def get_zillow_data(directory=os.getcwd(), filename="zillow.csv"):
+    """
+    This function will:
+    - Check local directory for csv file
+        - return if exists
+    - If csv doesn't exists:
+        - create a df of the SQL_query
+        - write df to csv
+    - Output zillow df
+"""
+    SQL_query = "select bedroomcnt, bathroomcnt, calculatedfinishedsquarefeet, taxvaluedollarcnt, yearbuilt, taxamount, fips from properties_2017 where propertylandusetypeid = '261'"
+    if os.path.exists(directory + filename):
+        df = pd.read_csv(filename) 
+        return df
+    
+    else:
+        url = env.get_db_url('zillow')
+    
+        df = pd.read_sql(SQL_query, url)
+        
+        #want to save to csv
+        df.to_csv(filename)
+        return df
+
+
+def remove_outliers(df, exclude_column=None, threshold=4):
+    """
+    This function removes outliers from a pandas dataframe, with an option to exclude a single column.
+    
+    Args:
+    df: pandas dataframe
+    exclude_column: string, optional column name to exclude from outlier detection
+    threshold: float, optional number of standard deviations from the mean to consider a value an outlier
+    
+    Returns:
+    pandas dataframe with outliers removed
+    """
+    if exclude_column is not None:
+        # Copy dataframe and drop excluded column
+        df_clean = df.drop(columns=exclude_column)
+    else:
+        df_clean = df.copy()
+    
+    # Calculate z-score for each value
+    z_scores = np.abs((df_clean - df_clean.mean()) / df_clean.std())
+    
+    # Remove rows with any value above threshold
+    df_clean = df.loc[(z_scores < threshold).all(axis=1)]
+    
+    return df_clean
+
+def remove_outliers(df, exclude_column=None, sd=4):
+    """
+    Remove outliers from a pandas DataFrame using the Z-score method.
+    
+    Args:
+    df (pandas.DataFrame): The DataFrame containing the data.
+    
+    Returns:
+    pandas.DataFrame: The DataFrame with outliers removed.
+    """
+    num_outliers_total = 0
+    for column in df.columns:
+        if column == exclude_column:
+            continue
+        series = df[column]
+        z_scores = np.abs(stats.zscore(series))
+        num_outliers = len(z_scores[z_scores > sd])
+        num_outliers_total += num_outliers
+        df = df[(z_scores <= sd) | pd.isnull(df[column])]
+        print(f"{num_outliers} outliers removed from {column}.")
+    print(f"\nTotal of {num_outliers_total} outliers removed.")
+    return df
+
+def plot_histograms(df):
+    """
+    Plots a histogram of each column in a pandas DataFrame using seaborn.
+    
+    Args:
+    df (pandas.DataFrame): The DataFrame containing the data.
+    """
+    # Loop through each column in the DataFrame
+    for col in df.columns:
+        # Create a histogram using seaborn
+        sns.histplot(data=df, x=col)
+        
+        # Show the plot
+        plt.show()
+
+def plot_boxplot(df):
+    """
+    Plots a histogram of each column in a pandas DataFrame using seaborn.
+    
+    Args:
+    df (pandas.DataFrame): The DataFrame containing the data.
+    """
+    # Loop through each column in the DataFrame
+    for col in df.columns:
+        # Create a histogram using seaborn
+        sns.boxplot(data=df, x=col)
+        
+        # Show the plot
+        plt.show()
+
+
+def split_data(df):
+    '''
+    Takes in two arguments the dataframe name and the ("stratify_name" - must be in string format) to stratify  and 
+    return train, validate, test subset dataframes will output train, validate, and test in that order
+    '''
+    train, test = train_test_split(df, #first split
+                                   test_size=.2, 
+                                   random_state=123)
+    train, validate = train_test_split(train, #second split
+                                    test_size=.25, 
+                                    random_state=123)
+    return train, validate, test
+
+def split_clean_zillow(df):
+    """
+    Remove nulls froms DataFrame.
+    
+    Args:
+    df (pandas.DataFrame): The DataFrame containing the data.
+    
+    Returns:
+    pandas.DataFrame: The DataFrame split into train, validate, test with nulls and outliers removed.
+    """
+    df = df.dropna()
+    df = df.drop(columns=["Unnamed: 0"])
+    # rename columns 
+    df = df.rename(columns={"bedroomcnt": "bedroom",
+                        	"bathroomcnt": "bathroom",
+                            "calculatedfinishedsquarefeet": "area",
+                            "taxvaluedollarcnt": "property_value",
+                            "yearbuilt": "year",
+                            "taxamount": "tax"})
+    df["fips"] = df.fips.map({6037: "LA", 6059: "Orange", 6111: "Ventura"})
+    df = remove_outliers(df,exclude_column="fips")
+    train, validate, test = split_data(df)
+    return train, validate, test
